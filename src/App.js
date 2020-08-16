211c5af8a5c976cd2052b675cbfc5af9c91f649b
Greg Allebach <greg.allebach@gmail.com>
2020-08-16 16:09:38 -0400

    formatted cards and started setting up header

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
 import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
+import CharacterCard from "./components/CharacterCard";
+import characters from "./characters.json";
 import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import friends from "./friends.json";
+import Header from "./components/Header";
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    characters
   };
 
   removeFriend = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
+    const characters = this.state.characters.filter(character => character.id !== id);
     // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
+    this.setState({ characters });
   };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
+      <div>
+      <Header />
       <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
+        {this.state.characters.map(character => (
+          <CharacterCard
+            id={character.id}
+            key={character.id}
+            image={character.image}
+            name={character.name}
           />
         ))}
       </Wrapper>
+      </div>
     );
   }
 }
